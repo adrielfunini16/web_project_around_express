1e4f3d68c1a13bb76228e1a6fd9f01496823c255
AdrielFunini <adrielfunini16@gmail.com>
2026-09-16 19:53:35 +0100

    fix: add handler for nonexistent routes

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const express = require("express");
 const app = express();
 const { PORT = 3000 } = process.env;
 
-app.get("/", (req, res) => {
-  res.status(404).send({ message: "A solicitação não foi encontrada" });
-});
-
 app.use("/cards", cardsRouter);
 
 app.use("/users", usersRouter);
+
+const notFoundHandler = (req, res) => {
+  res.status(404).send({ message: "A solicitação não foi encontrada" });
+};
+
+app.use(notFoundHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
